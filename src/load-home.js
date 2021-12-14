b1b6cff86a0d022a3f8f7e197b3c5874a956d644
Tyrmundr <taki.96061@gmail.com>
2021-12-14 10:34:22 +0100

    Added load module for homepage and cleaned up main JS file

---
+++ src/load-home.js
@@ -0,0 +1,21 @@
+"use strict"
+
+function loadHomePage() {
+    const content = document.getElementById("content");
+}
+
+function createHeader() {
+    const headerElement = document.createElement(header);
+}
+
+function createQuote() {
+    const quoteElement = document.createElement(div);
+}
+
+function createGallery() {
+    const galleryElement = document.createElement(div);
+}
+
+function createTestimonial() {
+    const testimonialElement = document.createElement(div)
+}
