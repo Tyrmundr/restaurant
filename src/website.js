1034008b0cdf1751481e5bb2a94a6228e98ac9ed
Tyrmundr <taki.96061@gmail.com>
2021-12-14 13:29:20 +0100

    Added a file that'll import all the different page modules and export an init function

---
+++ src/website.js
@@ -0,0 +1,19 @@
+import loadHome from "./load-home";
+
+function createMain() {
+    const main = document.createElement("main");
+    main.classList.add("main");
+    main.setAttribute("id", "main");
+
+    return main
+}
+
+function initializeWebsite() {
+    const content = document.getElementById("content");
+
+    content.appendChild(createMain());
+
+    loadHome();
+}
+
+export default initializeWebsite;
