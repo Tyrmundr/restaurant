1dd12bb81612e45f4ef2b52d21c94a86c0c07c03
Tyrmundr <taki.96061@gmail.com>
2021-12-15 11:00:54 +0100

    Finished the menu part and imported it into website - worked on website functions

--- src/load-menu.js
+++ src/load-menu.js
@@ -1,2 +1,37 @@
 "use strict"
 
+function createMenu() {
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+
+    menu.appendChild(createDish("Coconut & Squash dhansak", "butternut squash, onions, tomatoes, lentils, baby spinach, mini naan bread"));
+    menu.appendChild(createDish("Teriyaki salmon with sesame pack choi", "salmon fillet, noodles, pak choi, cloves, fish/vegetable stock"));
+    menu.appendChild(createDish("Apple pie", "white sugar, brown sugar, apples, unsalted butter"));
+
+    return menu;
+}
+
+function createDish(name, ingredients) {
+    const dishItem = document.createElement("div");
+    dishItem.classList.add("dish");
+    
+    const dishName = document.createElement("p");
+    dishName.classList.add("dish-name");
+    dishName.innerText = name;
+
+    const dishIngredients = document.createElement("p");
+    dishIngredients.classList.add("dish-ingredients");
+    dishIngredients.innerText = ingredients;
+
+    dishItem.appendChild(dishName);
+    dishItem.appendChild(dishIngredients);
+
+    return dishItem;
+}
+
+function loadMenu() {
+    const main = document.getElementById("main");
+    main.appendChild(createMenu());
+}
+
+export default loadMenu;
--- src/website.js
+++ src/website.js
@@ -1,4 +1,68 @@
 import loadHome from "./load-home";
+import loadMenu from "./load-menu";
+
+function createHeader() {
+    const header = document.createElement("header");
+    header.classList.add("header");
+
+    const restaurantName = document.createElement("h1");
+    restaurantName.classList.add("restaurant-name");
+    restaurantName.innerText = "The Hungry Bard"
+
+    header.appendChild(restaurantName);
+    header.appendChild(createNav());
+
+    return header;
+}
+
+function createNav() {
+    const nav = document.createElement("nav");
+    nav.classList.add("nav");
+
+    const btnHome = document.createElement("button");
+    btnHome.classList.add("btn-nav");
+    btnHome.innerText = "Home";
+    btnHome.addEventListener("click", (e) => {
+        if(e.target.classList.contains("active")) return;
+        setActiveBtn(btnHome);
+        loadHome();
+    })
+
+    const btnMenu = document.createElement("button");
+    btnMenu.classList.add("btn-nav");
+    btnMenu.innerText = "Menu";
+    btnMenu.addEventListener("click", (e) => {
+        if(e.target.classList.contains("active")) return;
+        setActiveBtn(btnMenu);
+        loadMenu;
+    })
+
+    const btnAbout = document.createElement("button");
+    btnAbout.classList.add("btn-nav");
+    btnAbout.innerText = "About";
+    const btnContact = document.createElement("button");
+    btnContact.classList.add("btn-nav");
+    btnContact.innerText = "Contact";
+
+    nav.appendChild(btnHome);
+    nav.appendChild(btnMenu);
+    nav.appendChild(btnAbout);
+    nav.appendChild(btnContact);
+
+    return nav;
+}
+
+function setActiveBtn(button) {
+    const buttons = document.querySelectorAll(".btn-nav");
+
+    buttons.forEach(button => {
+        if(button !== this) {
+            button.classList.remove("active");
+        }
+    });
+
+    button.classList.add("active");
+}
 
 function createMain() {
     const main = document.createElement("main");
@@ -11,6 +75,7 @@ function createMain() {
 function initializeWebsite() {
     const content = document.getElementById("content");
 
+    content.appendChild(createHeader());
     content.appendChild(createMain());
 
     loadHome();
